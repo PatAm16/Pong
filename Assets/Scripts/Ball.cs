e402e225e62f16b7a4974a0ff73649cb4691407b
PatAm16 <patricia.amaral16@gmail.com>
2021-03-22 13:01:22 +0000

    Update Pong.cs
    Bola em movimento e a colidir

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField]
+    float velocidade = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
